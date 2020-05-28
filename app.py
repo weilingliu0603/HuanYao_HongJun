7e5cc2ba512d84e7a8863adb75b4099e6a120daa
weilingliu0603 <63724387+weilingliu0603@users.noreply.github.com>
2020-05-28 15:45:26 +0800

    Update and rename JPJC Enterprise Server.py to app.py

--- app.py
+++ app.py
@@ -90,10 +90,9 @@ def viewinventory():
     db.close()
     return flask.render_template('viewinventory.html', rows=rows)
 
-if __name__ == '__main__':
-    app.run(port = 33333, debug=True)
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
 
 
 
